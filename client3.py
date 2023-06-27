eb587370647f83f86679d478465b3195c987aae0
Akshaj Murhekar <akshaj.murhekar@mavs.uta.edu>
2023-06-27 15:23:31 -0500

    task 1

--- client3.py
+++ client3.py
@@ -35,14 +35,19 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # price is the average of bid and ask
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # avoiding the divide by zero error.
+    if(price_b == 0):
+        return
+    # returning the ratio of price_a and price_b
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +57,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        stockPrices = {}
+
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            stockPrices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(stockPrices['ABC'], stockPrices['DEF']))
